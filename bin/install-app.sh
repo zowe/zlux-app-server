66b514dd9b40880e8326229b2cf95ad800433ac1
Fyodor Kovin <fkovin@rocketsoftware.com>
2019-06-24 12:43:32 -0400

    Simplify the way we deal with pathnames
    Signed-off-by: Fyodor Kovin <fkovin@rocketsoftware.com>

--- bin/install-app.sh
+++ bin/install-app.sh
@@ -6,29 +6,22 @@
 # SPDX-License-Identifier: EPL-2.0
 # 
 # Copyright Contributors to the Zowe Project.
-start_script=$0
-start_path=$(pwd)
-script_path=`dirname "${start_script}"`
-cd $script_path
-script_path=$(pwd)
 if [ $# -eq 0 ]
     then
     echo "Usage: $0 AppPath"
     exit 1
 fi
 
-utils_path=${script_path}/../../zlux-server-framework/utils
-json_path=${script_path}/../../zlux-app-server/deploy/instance/ZLUX/serverConfig/zluxserver.json
-cd $start_path
-cd $1
-app_path=$(pwd)
-cd $script_path
+zlux_path=$(cd $(dirname "$0")/../..; pwd)
+utils_path=$zlux_path/zlux-server-framework/utils
+json_path=$zlux_path/zlux-app-server/deploy/instance/ZLUX/serverConfig/zluxserver.json
+app_path=$(cd "$1"; pwd)
 
 shift
 
 if [ -z "$ZLUX_INSTALL_LOG_DIR" ]
     then
-    ZLUX_INSTALL_LOG_DIR="../log"
+    ZLUX_INSTALL_LOG_DIR="$zlux_path/zlux-app-server/log"
 fi
 
 if [ ! -d "$ZLUX_INSTALL_LOG_DIR" ]
@@ -38,10 +31,9 @@ if [ ! -d "$ZLUX_INSTALL_LOG_DIR" ]
 fi
 
 LOG_FILE="$ZLUX_INSTALL_LOG_DIR/install.log"
-echo "start_path=${start_path}\nutils_path=${utils_path}\napp_path=${app_path}"
+echo "utils_path=${utils_path}\napp_path=${app_path}"
 echo "Running installer. Log location=$LOG_FILE"
 node ${utils_path}/install-app.js -i "$app_path" -c "$json_path" $@ 2>&1 | tee $LOG_FILE
-cd $start_path
 
 # This program and the accompanying materials are
 # made available under the terms of the Eclipse Public License v2.0 which accompanies

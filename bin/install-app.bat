d249ae9442cfc132a1c11ef03b9a667c5ced0bbc
1000TurquoisePogs <sgrady@rocketsoftware.com>
2019-10-17 21:16:37 -0400

    Updates to batch, initialization script
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- bin/install-app.bat
+++ bin/install-app.bat
@@ -13,6 +13,10 @@ set app_path="%~f1"
 if "%ZLUX_INSTALL_LOG_DIR%" == "" (
   set ZLUX_INSTALL_LOG_DIR=..\log
 )
+if "%ZLUX_CONFIG_FILE%" == "" (
+  set ZLUX_CONFIG_FILE=%~dp0\..\..\zlux-app-server\deploy\instance\ZLUX\serverConfig\zluxserver.json
+)
+
 call :makedir %ZLUX_INSTALL_LOG_DIR%
 call :abspath %ZLUX_INSTALL_LOG_DIR%\install.log
 set LOG_PATH=%RETVAL%
@@ -20,7 +24,7 @@ echo Checking for node
 where node
 if %ERRORLEVEL% neq 0 goto :nonode
 echo Running installer. Log location=%LOG_PATH%
-node "%~dp0..\..\zlux-server-framework\utils\install-app.js" -i "%app_path%"  -o "%~dp0..\..\\" -c "%~dp0..\..\zlux-app-server\deploy\instance\ZLUX\serverConfig\zluxserver.json" %2 > %LOG_PATH% 2>&1
+node "%~dp0..\..\zlux-server-framework\utils\install-app.js" -i "%app_path%"  -o "%~dp0..\..\\" -c "%ZLUX_CONFIG_FILE%" %2 > %LOG_PATH% 2>&1
 set rc=%ERRORLEVEL%
 echo Ended with rc=%rc%
 endlocal
--- bin/nodeServer.bat
+++ bin/nodeServer.bat
@@ -6,38 +6,39 @@ REM
 REM SPDX-License-Identifier: EPL-2.0
 REM 
 REM Copyright Contributors to the Zowe Project.
-
 setlocal
-if "%ZLUX_NODE_LOG_DIR%" == "" (
-  set ZLUX_NODE_LOG_DIR=..\log
+if not "%ZLUX_NODE_LOG_FILE%" == "" (
+  FOR /F %%i IN ("%ZLUX_NODE_LOG_FILE%") DO set ZLUX_LOG_PATH=%%~fi  
+) else (
+  if "%ZLUX_NODE_LOG_DIR%" == "" (
+    set ZLUX_NODE_LOG_DIR="../log"
+  )
+  call :makedir "%ZLUX_NODE_LOG_DIR%"
+  FOR /F %%i IN ("%ZLUX_NODE_LOG_DIR%\nodeServer.log") DO set ZLUX_LOG_PATH=%%~fi  
 )
-call :makedir %ZLUX_NODE_LOG_DIR%
+if "%ZLUX_CONFIG_FILE%" == "" (
+  set ZLUX_CONFIG_FILE=../defaults/serverConfig/server.json
+)
 set NODE_PATH=../..;../../zlux-server-framework/node_modules;%NODE_PATH%
 cd ../lib
-call :abspath %ZLUX_NODE_LOG_DIR%\nodeServer.log
-set ZLUX_LOG_PATH=%RETVAL%
-
 REM Check if print to terminal argument exists
 echo.%* | findstr /C:"--logToTerminal" 1>nul
 if errorlevel 1 (
-  echo Server startup. Log location=%LOG_PATH%
-  node --harmony zluxServer.js --config=../defaults/config/server.json %* > %ZLUX_LOG_PATH% 2>&1
+  echo Server startup. Log location=%ZLUX_LOG_PATH%
+  node --harmony zluxServer.js --config="%ZLUX_CONFIG_FILE%" %* > %ZLUX_LOG_PATH% 2>&1
 ) ELSE (
   echo Server startup. Logging to terminal...
-  node --harmony zluxServer.js --config=../defaults/serverConfig/server.json %*
+  node --harmony zluxServer.js --config="%ZLUX_CONFIG_FILE%" %*
 )
-echo Ended with rc=%ERRORLEVEL%
+set rc=%ERRORLEVEL%
+echo Ended with rc=%rc%
 endlocal
-goto :eof
+exit %rc%
 
 REM Create a directory if it does not exist yet
 :makedir
-if not exist %1 mkdir %1
+if not exist "%1" mkdir "%1"
 goto :eof
-
-:abspath
-set RETVAL=%~dpfn1
-exit /B
 REM This program and the accompanying materials are
 REM made available under the terms of the Eclipse Public License v2.0 which accompanies
 REM this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html

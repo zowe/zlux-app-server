88ef6cb43cd0550bca057af4e73cb317470c8d05
1000TurquoisePogs <sgrady@rocketsoftware.com>
2019-12-05 10:46:43 -0500

    Fixes for install-app
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- bin/install-app.sh
+++ bin/install-app.sh
@@ -11,7 +11,6 @@ if [ $# -eq 0 ]
     echo "Usage: $0 AppPath [PluginsDir]"
     exit 1
 fi
-
 if [ -n "$NODE_HOME" ]
 then
   NODE_BIN=${NODE_HOME}/bin/node
@@ -21,62 +20,65 @@ fi
 
 zlux_path=$(cd $(dirname "$0")/../..; pwd)
 utils_path=$zlux_path/zlux-server-framework/utils
-if [ -z $2 ]
+app_path=$(cd "$1"; pwd)
+if [ $# -gt 1 ]
 then
-  if [ -n $WORKSPACE_DIR ]
+  plugin_dir=$2
+  shift
+fi
+shift
+
+if [ -z "$plugin_dir" ]
+then
+  if [ -e "${WORKSPACE_DIR}/app-server/serverConfig/server.json" ]
   then
-    json_path=$WORKSPACE_DIR/app-server/serverConfig/server.json
+    json_path=${WORKSPACE_DIR}/app-server/serverConfig/server.json
   else
     json_path=$zlux_path/zlux-app-server/defaults/serverConfig/server.json
   fi
 fi
 
-app_path=$(cd "$1"; pwd)
-
-shift
 
 cd $zlux_path/zlux-app-server/bin
 
 if [ -z "$ZLUX_INSTALL_LOG_DIR" ]
 then
-  if [ -n $INSTANCE_DIR ]
+  if [ -d "${INSTANCE_DIR}/logs" ]
   then
     ZLUX_INSTALL_LOG_DIR="$INSTANCE_DIR/logs"
-  else
-    ZLUX_INSTALL_LOG_DIR="$zlux_path/zlux-app-server/log"
   fi
 fi
 
-if [ ! -d "$ZLUX_INSTALL_LOG_DIR" ]
-   then
-   echo "Will make log directory $ZLUX_INSTALL_LOG_DIR"
-   mkdir -p $ZLUX_INSTALL_LOG_DIR
+if [ ! -z "$ZLUX_INSTALL_LOG_DIR" ]
+then
+  if [ ! -d "$ZLUX_INSTALL_LOG_DIR" ]
+  then
+     echo "Will make log directory $ZLUX_INSTALL_LOG_DIR"
+     mkdir -p $ZLUX_INSTALL_LOG_DIR
+  fi
+  PLUGIN_LOG_FILE="$ZLUX_INSTALL_LOG_DIR/install-app.log"
+  LOG_CMD="| tee $PLUGIN_LOG_FILE"
 fi
 
-LOG_FILE="$ZLUX_INSTALL_LOG_DIR/install-app.log"
-echo "utils_path=${utils_path}\napp_path=${app_path}"
-echo "Checking for node"
-type node
+
+echo "Verifying node exists"
+type ${NODE_BIN}
 rc=$?
 if [ $rc -ne 0 ]
     then
     echo "Node required for installation. Add to PATH and try again"
     exit $rc
 fi
-echo "Running installer. Log location=$LOG_FILE"
-if [ -n $2 ]
+echo "Running app-server plugin installer. Log=$PLUGIN_LOG_FILE"
+echo "utils_path=${utils_path}\napp_path=${app_path}"
+if [ -d "$plugin_dir" ]
 then
-__UNTAGGED_READ_MODE=V6 ${NODE_BIN} ${utils_path}/install-app.js -i "$app_path" -p "$2" $@ 2>&1 | tee $LOG_FILE
+  echo "plugin_dir=${plugin_dir}"
+__UNTAGGED_READ_MODE=V6 ${NODE_BIN} ${utils_path}/install-app.js -i "$app_path" -p "$plugin_dir" $@ 2>&1 $LOG_CMD
 else
-__UNTAGGED_READ_MODE=V6 ${NODE_BIN} ${utils_path}/install-app.js -i "$app_path" -c "$json_path" $@ 2>&1 | tee $LOG_FILE
+  echo "json_path=${json_path}"
+__UNTAGGED_READ_MODE=V6 ${NODE_BIN} ${utils_path}/install-app.js -i "$app_path" -c "$json_path" $@ 2>&1 $LOG_CMD
 fi
 rc=$?
-echo "Ended with rc=${rc}"
+echo "Plugin install completed with rc=${rc}"
 exit $rc
-# This program and the accompanying materials are
-# made available under the terms of the Eclipse Public License v2.0 which accompanies
-# this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-# 
-# SPDX-License-Identifier: EPL-2.0
-# 
-# Copyright Contributors to the Zowe Project.

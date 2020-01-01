d02eb7653c69885830defc803fde605aed598491
1000TurquoisePogs <sgrady@rocketsoftware.com>
2020-01-01 15:01:27 -0500

    Adding another missing dollar sign, allow for mediation layer use automatically
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- bin/convert-env.sh
+++ bin/convert-env.sh
@@ -25,11 +25,20 @@ then
     export ZWED_node_mediationLayer_server_port=$DISCOVERY_PORT
   fi
 fi
-if [ -z "ZWED_node_mediationLayer_server_hostname" ] 
+if [ -z "$ZWED_node_mediationLayer_server_hostname" ] 
 then
   if [ -n "$ZOWE_EXPLORER_HOST" ]
   then
     export ZWED_node_mediationLayer_server_hostname=$ZOWE_EXPLORER_HOST
+    if [ -n "$ZWED_node_mediationLayer_server_port" ]
+    then
+      case "$LAUNCH_COMPONENT_GROUPS" in
+        *GATEWAY*)
+          #All conditions met for app-server behind gateway: hostname, port, and component
+          export ZWED_node_mediationLayer_enabled="true"
+          ;;
+      esac
+    fi
   fi
 fi
 

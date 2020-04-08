934fb9c3b8bdbcb816138a9158db9e0e019a00fc
1000TurquoisePogs <sgrady@rocketsoftware.com>
2020-04-08 16:45:48 -0400

    Cannot use --title in node older than v10
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- bin/app-server.sh
+++ bin/app-server.sh
@@ -244,5 +244,4 @@ then
 fi
 JOBNAME=${ZOWE_PREFIX}DS${ZOWE_INSTANCE}
 
-{ __UNTAGGED_READ_MODE=V6 _BPX_JOBNAME=${JOBNAME} ${NODE_BIN} --harmony --title "${JOBNAME}" ${ZLUX_SERVER_FILE} --config="${CONFIG_FILE}" "$@" 2>&1 ; echo "Ended with rc=$?" ; } | tee $ZLUX_NODE_LOG_FILE
-
+{ __UNTAGGED_READ_MODE=V6 _BPX_JOBNAME=${JOBNAME} ${NODE_BIN} --harmony "${JOBNAME}" ${ZLUX_SERVER_FILE} --config="${CONFIG_FILE}" "$@" 2>&1 ; echo "Ended with rc=$?" ; } | tee $ZLUX_NODE_LOG_FILE

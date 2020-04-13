85a9bc7a38d4239f3f2b99191b2105f0e59ad34d
1000TurquoisePogs <sgrady@rocketsoftware.com>
2020-04-13 15:09:56 -0400

    more title work
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- bin/app-server.sh
+++ bin/app-server.sh
@@ -242,6 +242,6 @@ if [ -z "$ZOWE_INSTANCE" ]
 then
     ZOWE_INSTANCE="1"
 fi
-JOBNAME=${ZOWE_PREFIX}DS${ZOWE_INSTANCE}
+export ZLUX_JOBNAME=${ZOWE_PREFIX}DS${ZOWE_INSTANCE}
 
-{ __UNTAGGED_READ_MODE=V6 _BPX_JOBNAME=${JOBNAME} ${NODE_BIN} --harmony "${JOBNAME}" ${ZLUX_SERVER_FILE} --config="${CONFIG_FILE}" "$@" 2>&1 ; echo "Ended with rc=$?" ; } | tee $ZLUX_NODE_LOG_FILE
+{ __UNTAGGED_READ_MODE=V6 _BPX_JOBNAME=${ZLUX_JOBNAME} ${NODE_BIN} --harmony ${ZLUX_SERVER_FILE} --config="${CONFIG_FILE}" "$@" 2>&1 ; echo "Ended with rc=$?" ; } | tee $ZLUX_NODE_LOG_FILE

f0013bd105bbf8fe921bbbf683c8ea04d2f67c2c
Timothy Gerstel <tgerstel@rocketsoftware.com>
2020-10-15 14:22:36 -0400

    Add external certificate authorities to app server CA array
    Signed-off-by: Timothy Gerstel <tgerstel@rocketsoftware.com>

--- bin/convert-env.sh
+++ bin/convert-env.sh
@@ -65,7 +65,7 @@ then
   elif [ -n "$KEYSTORE_CERTIFICATE" ]
   then
     #, at end turns it into an array
-    export ZWED_node_https_certificates=$KEYSTORE_CERTIFICATE,$KEYSTORE,
+    export ZWED_node_https_certificates=$KEYSTORE_CERTIFICATE,
   fi
 fi
 if [ -z "$ZWED_node_https_certificateAuthorities" ]
@@ -77,7 +77,12 @@ then
   elif [ -n "$KEYSTORE_CERTIFICATE_AUTHORITY" ]
   then
     #, at end turns it into an array
-    export ZWED_node_https_certificateAuthorities=${KEYSTORE_CERTIFICATE_AUTHORITY},${EXTERNAL_ROOT_CA},$KEYSTORE,
+    if [ -n "$EXTERNAL_CERTIFICATE_AUTHORITIES" ]
+    then
+      export ZWED_node_https_certificateAuthorities=${KEYSTORE_CERTIFICATE_AUTHORITY},${EXTERNAL_ROOT_CA},${$EXTERNAL_CERTIFICATE_AUTHORITIES// /,}
+    else
+      export ZWED_node_https_certificateAuthorities=${KEYSTORE_CERTIFICATE_AUTHORITY},${EXTERNAL_ROOT_CA},
+    fi
   fi
 fi
 if [ -z "$ZWED_node_https_keys" ]

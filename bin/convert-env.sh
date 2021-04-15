5a0a87be0519e35e034f60a07115249f43efb584
1000TurquoisePogs <sgrady@rocketsoftware.com>
2021-04-15 11:45:50 -0400

    Fix typo
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- bin/convert-env.sh
+++ bin/convert-env.sh
@@ -52,8 +52,8 @@ then
 fi
 if [ -z "$ZWED_node_mediationLayer_enabled" ]; then
   export ZWED_node_mediationLayer_enabled="false"
-elif [ -z "$ZWED_agent_mediationLayer_enabled" ]; then
-  export ZWED_agent_mediatonLayer_enabled="true";
+elif [ -n "$ZWED_agent_mediationLayer_serviceName" -a -z "$ZWED_agent_mediationLayer_enabled" ]; then
+  export ZWED_agent_mediationLayer_enabled="true";
 fi
 
 # Check if Caching Service is enabled

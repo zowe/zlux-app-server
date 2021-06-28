d6d7bfd070981b64b0a034c3d55b16d908ff10e2
Nakul Manchanda <nakul.manchanda@ibm.com>
2021-06-28 14:54:39 -0400

    update install container paths
    Signed-off-by: Nakul Manchanda <nakul.manchanda@ibm.com>

--- bin/install-container.sh
+++ bin/install-container.sh
@@ -1,19 +1,23 @@
-# create bin directory
-CONTAINER_SCRIPTS=${INSTALL_DIR}/files/scripts
-
 #RUNTIME PATHS
 ZLUX_RUNTIME=${ROOT_DIR}/components/app-server
 ZLUX_APP_SERVER=${ZLUX_RUNTIME}/share/zlux-app-server
 
-#INSTANCE PATHS APP-SERVER workspace
-ZLUX_WORKSPACE=${WORKSPACE_DIR}/app-server
-
+# create runtime bin
 mkdir -p ${ZLUX_RUNTIME}/bin 
 cp ${ZLUX_APP_SERVER}/manifest.yaml ${ZLUX_RUNTIME} 
 cp ${ZLUX_APP_SERVER}/bin/start.sh ${ZLUX_RUNTIME}/bin 
 cp ${ZLUX_APP_SERVER}/bin/configure.sh ${ZLUX_RUNTIME}/bin
+cp ${ZLUX_APP_SERVER}/bin/start-container.sh ${ZLUX_RUNTIME}/bin 
 
+#INSTANCE PATHS 
+ZLUX_WORKSPACE=${WORKSPACE_DIR}/app-server
+
+#create app-server workspace
 mkdir -p ${ZLUX_WORKSPACE} 
-cp ${CONTAINER_SCRIPTS}/start-container.sh ${ZLUX_RUNTIME}/bin 
-cd ${CONTAINER_SCRIPTS} && ./configure-container.sh
+
+# configure app-server workspace
+cd ${ZLUX_APP_SERVER}/bin
+./configure-container.sh
+
+#cleanup
 rm -rf ${INSTALL_DIR}/files

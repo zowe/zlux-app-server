0b5e23ff9e0d168e079fc98a419785087ab6f032
Aditya Ranshinge <aranshinge@rocketsoftware.com>
2022-03-23 22:12:11 +0530

    Encoding issue fix
    Signed-off-by: Aditya Ranshinge <aranshinge@rocketsoftware.com>

--- bin/app-server.sh
+++ bin/app-server.sh
@@ -194,7 +194,7 @@ else
 fi
 
 __UNTAGGED_READ_MODE=V6 _BPX_JOBNAME=${ZOWE_PREFIX}DS1 ${NODE_BIN} --harmony ${ZOWE_LIB_DIR}/${ZLUX_SERVER_FILE} --config="${CONFIG_FILE}" "$@" 2>&1 | tee $ZWED_NODE_LOG_FILE
-
+echo "Ended with rc=$?" | tee $ZWED_NODE_LOG_FILE
 
 
 

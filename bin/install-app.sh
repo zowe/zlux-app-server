aa77747a021e6c7585a7f019254b752ea0c5724b
Aditya Ranshinge <aranshinge@rocketsoftware.com>
2022-04-08 19:09:59 +0530

    containers env fix
    Signed-off-by: Aditya Ranshinge <aranshinge@rocketsoftware.com>

--- bin/install-app.sh
+++ bin/install-app.sh
@@ -20,8 +20,6 @@ setVars() {
   umask 0002                                       # similar to chmod 755
   . ${zlux_path}/zlux-app-server/bin/init/node-init.sh
 }
-echo "**** install-app.sh stderr ****" >&2
-echo "**** install-app.sh stdout ****"
 
 dir=$(cd `dirname $0` && pwd)
 . ${dir}/utils/plugin-utils.sh
@@ -32,17 +30,16 @@ then
   COMPONENT_HOME=${ZWE_zowe_runtimeDirectory}/components/app-server
 
   # containers only
-  if [ ! -f "${COMPONENT_HOME}/manifest.yaml" ]; then
+  if [ "${ZWE_RUN_ON_ZOS}" != "true" ]; then
     if [ -f "/component/manifest.yaml" -o -f "/component/manifest.json" -o -f "/component/manifest.yml" ]; then
       COMPONENT_HOME=/component
-      ZLUX_CONTAINER_MODE=1
+      ZLUX_CONTAINER_MODE=1  
       INSTALL_NO_NODE=1  
     fi
   fi
 
   if [ -z "$INSTALL_NO_NODE" ]; then
     zlux_path="$COMPONENT_HOME/share"
-    echo "**** INSTALL_NO_NODE=$INSTALL_NO_NODE zlux_path=$zlux_path" >&2
     setVars
     if [ ! -d "${ZWE_zowe_workspaceDirectory}/app-server" ]
     then
@@ -62,16 +59,12 @@ fi
 
 utils_path=$zlux_path/zlux-server-framework/utils
 app_path=$(cd "$1"; pwd)
-
-echo "**** utils_path=$utils_path app_path=$app_path" >&2
-
 if [ $# -gt 1 ]
 then
-  echo "**** plugin_dir 1" >&2
   plugin_dir=$2
   shift
 else
-  echo "**** plugin_dir 2" >&2
+  getPluginsDir
   plugin_dir=$(getPluginsDir)
 fi
 shift
@@ -81,30 +74,28 @@ if [ -z "$plugin_dir" ]; then
   echo "Plugin registration ended with rc=1"
   exit 1
 fi
-echo "**** plugin_dir=$plugin_dir" >&2
-
 mkdir -p $plugin_dir
 
 # Installs a zowe plugin by finding its ID and writing the locator json WITHOUT using install-app.js
 # This is to be used in cases where there are issues using JS, or nodejs is not found.
 # Input: relative or fully qualified path to a directory containing a plugindir=$(cd `dirname $0` && pwd)
 installNojs() {
-  echo "**** inside installNojs" >&2
-
   id=$(getPluginID "${app_path}")
   if [ -n "${id}" ]
   then
     echo "Found plugin=${id}"
-    echo "**** id=$id" >&2
-    ls ${plugin_dir} >&2
+
 cat <<EOF >${plugin_dir}/${id}.json
 {
   "identifier": "${id}",
   "pluginLocation": "${app_path}"
 }
 EOF
-    echo "Plugin registration ended with rc=$?" >&2
-    chmod 0750 "${plugin_dir}/${id}.json"
+    echo "Plugin registration ended with rc=$?"
+    if [ -f "${plugin_dir}/${id}.json" ]
+    then
+      chmod 0750 "${plugin_dir}/${id}.json"
+    fi
   else
       echo "Error: could not find plugin id for path=${app_path}"
       exit 1
@@ -113,7 +104,6 @@ EOF
 
 if [ -n "$INSTALL_NO_NODE" ]
 then
- echo "**** INSTALL_NO_NODE1=$INSTALL_NO_NODE" >&2
  installNojs
 else  
   cd $zlux_path/zlux-app-server/bin
@@ -123,12 +113,9 @@ else
   rc=$?
   if [ $rc -ne 0 ]
   then
-    echo "**** INSTALL_NO_NODE2" >&2
     installNojs
   else
 # normal case follows
-echo "**** Normal case" >&2
-
 if [ -z "$ZLUX_INSTALL_LOG_DIR" ]
 then
   if [ -d "${ZWE_zowe_logDirectory}" ]
@@ -136,7 +123,6 @@ then
     ZLUX_INSTALL_LOG_DIR="$ZWE_zowe_logDirectory"
   fi
 fi
-echo "**** ZLUX_INSTALL_LOG_DIR=$ZLUX_INSTALL_LOG_DIR" >&2
 
 PLUGIN_LOG_FILE=/dev/null
 if [ ! -z "$ZLUX_INSTALL_LOG_DIR" ]
@@ -149,7 +135,6 @@ then
   PLUGIN_LOG_FILE="$ZLUX_INSTALL_LOG_DIR/install-app.log"
 fi
 
-echo "**** PLUGIN_LOG_FILE=$PLUGIN_LOG_FILE" >&2
 
 echo "Running app-server plugin registration. Log=$PLUGIN_LOG_FILE"
 echo "utils_path=${utils_path}\napp_path=${app_path}"

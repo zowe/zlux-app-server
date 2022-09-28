75a8919fba8f31b6ad6a6e90c1fe78d44959ee0d
1000TurquoisePogs <sgrady@rocketsoftware.com>
2022-09-28 13:25:37 -0400

    Accomodate app2app in configmgr use
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- bin/init/plugins-init.js
+++ bin/init/plugins-init.js
@@ -14,12 +14,15 @@ import * as std from 'std';
 import * as xplatform from 'xplatform';
 import * as fs from '../../../../../../bin/libs/fs';
 import * as componentlib from '../../../../../../bin/libs/component';
+import { PathAPI as pathoid } from '../../../../../../bin/libs/pathoid';
 
 console.log(`Started plugins-init.js, platform=${os.platform}`);
 
 const runtimeDirectory=std.getenv('ZWE_zowe_runtimeDirectory');
 const extensionDirectory=std.getenv('ZWE_zowe_extensionDirectory');
 const workspaceDirectory=std.getenv('ZWE_zowe_workspaceDirectory');
+const recognizerDirectory=workspaceDirectory+'/app-server/ZLUX/pluginStorage/org.zowe.zlux.ng2desktop/recognizers';
+const actionsDirectory=workspaceDirectory+'/app-server/ZLUX/pluginStorage/org.zowe.zlux.ng2desktop/actions';
 
 const installedComponentsEnv=std.getenv('ZWE_INSTALLED_COMPONENTS');
 const installedComponents = installedComponentsEnv ? installedComponentsEnv.split(',') : null;
@@ -46,18 +49,26 @@ function registerPlugin(pluginPath, pluginDefinition) {
         location = location.substring(1);
       }
 
-      return xplatform.storeFileUTF8(pointerPath, xplatform.AUTO_DETECT, JSON.stringify({
+      xplatform.storeFileUTF8(pointerPath, xplatform.AUTO_DETECT, JSON.stringify({
         "identifier": pluginDefinition.identifier,
         "pluginLocation": location,
         "relativeTo": relativeTo
       }, null, 2));
     } else {
-      return xplatform.storeFileUTF8(pointerPath, xplatform.AUTO_DETECT, JSON.stringify({
+      xplatform.storeFileUTF8(pointerPath, xplatform.AUTO_DETECT, JSON.stringify({
         "identifier": pluginDefinition.identifier,
         "pluginLocation": pluginPath
       }, null, 2));
     }
+    registerApp2App(pluginPath, pluginDefinition.identifier, pluginDefinition.pluginVersion);
   }
+}
+
+
+
+function registerApp2App(pluginDirectory, pluginId, pluginVersion) {
+  copyRecognizers(pluginDirectory, pluginId, pluginVersion);
+  copyActions(pluginDirectory, pluginId, pluginVersion);
 }
 
 function deregisterPlugin(pluginDefinition) {
@@ -69,9 +80,99 @@ function deregisterPlugin(pluginDefinition) {
     }
     return rc !== 0;
   } else {
-    return true;
+    return deregisterApp2App(pluginDefinition.identifier);
   }
 }
+
+function deregisterApp2App(appId) {
+  const actionPath = path.join(actionsDirectory, appId);
+  if (fs.fileExists(actionPath, true)) {
+    const rc = deleteFile(actionPath);
+    if (rc !== 0) {
+      console.log(`Could not deregister plugin ${appId}, delete ${actionPath} failed, error=${rc}`);
+    }
+    return rc === 0;
+  }
+  //TODO how to deregister recognizer?
+}
+
+function copyRecognizers(appDir, appId, appVers) {
+  let recognizers;
+  let recognizersKeys;
+  let configRecognizers;
+  const pluginRecognizersLocation = pathoid.join(appDir, "config", "recognizers");
+
+
+  if (fs.directoryExists(pluginRecognizersLocation)) { // Get recognizers in a plugin's appDir/config/xxx location
+    fs.getFilesInDirectory(pluginRecognizersLocation).forEach(filename => {
+      const filepath = pathoid.resolve(pluginRecognizersLocation, filename);
+      const filepathConfig = path.resolve(path.join(recognizerDirectory, filename));
+      
+      recognizers = JSON.parse(xplatform.loadFileUTF8(filepath, xplatform.AUTO_DETECT)).recognizers;
+      recognizersKeys = Object.keys(recognizers)
+      for (const key of recognizersKeys) { // Add metadata for plugin version & plugin identifier of origin (though objects don't have to be plugin specific)
+        recognizers[key].pluginVersion = appVers;
+        recognizers[key].pluginIdentifier = appId;
+        recognizers[key].key = appId + ":" + key + ":" + recognizers[key].id; // pluginid_that_provided_it:index(or_name)_in_that_provider:actionid
+      }
+      console.log(`ZWED0301I Found ${recognizers} in config for '${appId}'`);
+      try { // Get pre-existing recognizers in config, if any
+        configRecognizers = JSON.parse(xplatform.loadFileUTF8(filepathConfig, xplatform.AUTO_DETECT)).recognizers;
+        const configRecognizersKeys = Object.keys(configRecognizers);
+        for (const configKey of configRecognizersKeys) { // Traverse config recognizers
+          for (const key of recognizerKeys) { // Traverse plugin recognizers
+            if (configRecognizers[configKey].key && recognizers[key].key && configRecognizers[configKey].key == recognizers[key].key) { // TODO: Need to implement real keys for Recognizers
+              configRecognizers[configKey] = recognizers[key]; // Choose the recognizers originating from plugin
+            }
+          }
+        }
+        recognizers = Object.assign(configRecognizers, recognizers); // // If found, combine the ones found in config with ones found in plugin
+      } catch (e) {
+        logger.debug("No existing recognizers were found in config for '" + appId + "'");
+      }
+      
+      if (recognizers) { // Attempt to copy recognizers over to config location for Desktop access later
+        try { //TODO: Doing recognizers.recognizers is redundant. We may want to consider refactoring in the future
+          xplatform.storeFileUTF8(filepathConfig, xplatform.AUTO_DETECT,  '{ "recognizers":' + JSON.stringify(recognizers) + '}');
+          console.log("ZWED0294I Successfully loaded " + recognizers.length + " recognizers for '" + appId + "' into config");
+        } catch (e) {
+          console.log(`ZWED0177W Unable to load ${recognizers} for '${appId}' into config`);
+        }
+      }
+      
+    });
+  }
+}
+
+function copyActions(appDir, appId, appVers) {
+  let actions;
+  let actionsKeys;
+  const pluginActionsLocation = pathoid.join(appDir, "config", "actions", appId);
+
+  if (fs.fileExists(pluginActionsLocation)) {
+    try { // Get actions in a plugin's appDir/config/xxx location
+      actions = JSON.parse(xplatform.loadFileUTF8(pluginActionsLocation, xplatform.AUTO_DETECT)).actions;
+      actionsKeys = Object.keys(actions)
+      for (const key of actionsKeys) { // Add metadata for plugin version & plugin identifier of origin (though objects don't have to be plugin specific)
+        actions[key].pluginVersion = appVers;
+        actions[key].pluginIdentifier = appId;
+      }
+      console.log(`ZWED0301I Found ${actions} in config for '${appId}'`);
+    } catch (e) {
+      console.log(`Error: Malformed JSON in ${pluginActionsLocation}`);
+    }
+
+    if (actions) { // Attempt to copy actions over to config location for Desktop access later
+      try { //TODO: Doing actions.actions is redundant. We may want to consider refactoring in the future
+        xplatform.storeFileUTF8(pathoid.join(actionsDirectory, appId), xplatform.AUTO_DETECT,  '{ "actions":' + JSON.stringify(actions) + '}');
+        console.log("ZWED0295I Successfully loaded " + actions.length + " actions for '" + appId + "' into config");
+      } catch (e) {
+        console.log(`ZWED0177W Unable to load ${actions} for '${appId}' into config`);
+      }
+    }
+  }
+}
+
 
 if (!fs.directoryExists(pluginPointerDirectory, true)) {
   const rc = os.mkdir(pluginPointerDirectory, 0o770);

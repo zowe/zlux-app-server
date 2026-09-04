8bb281a2e54b78224bbb3ddbbee2a3bf346dabf5
Martin Zeithaml <66114686+Martin-Zeithaml@users.noreply.github.com>
2026-09-04 11:15:37 +0200

    `env` only in debug or trace mode- #404
    Signed-off-by: Martin Zeithaml <66114686+Martin-Zeithaml@users.noreply.github.com>

--- bin/start.sh
+++ bin/start.sh
@@ -82,8 +82,10 @@ fi
 # set production mode if applicable
 export NODE_ENV=${NODE_ENV:-production}
 
-echo Show Environment
-env | grep -i -v password | grep -i -v secret
+if [ "${ZWE_PRIVATE_LOG_LEVEL_ZWELS}" = "DEBUG" -o "${ZWE_PRIVATE_LOG_LEVEL_ZWELS}" = "TRACE" ]; then
+  echo Show Environment
+  env | awk -F= '{ if (toupper($1) !~ /PASSWORD|PASSPHRASE|SECRET|TOKEN|KEY|CRED/) print }'
+fi
 
 cd ${ZLUX_APP_SERVER_DIR}/lib
 echo Starting node

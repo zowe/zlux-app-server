ac253502c18faea3c6155c2a4c7ad4582c846d48
Martin Zeithaml <66114686+Martin-Zeithaml@users.noreply.github.com>
2026-09-07 09:23:47 +0200

    Print env always
    Signed-off-by: Martin Zeithaml <66114686+Martin-Zeithaml@users.noreply.github.com>

--- bin/start.sh
+++ bin/start.sh
@@ -82,10 +82,8 @@ fi
 # set production mode if applicable
 export NODE_ENV=${NODE_ENV:-production}
 
-if [ "${ZWE_PRIVATE_LOG_LEVEL_ZWELS}" = "DEBUG" -o "${ZWE_PRIVATE_LOG_LEVEL_ZWELS}" = "TRACE" ]; then
-  echo Show Environment
-  env | awk -F= '{ if (toupper($1) !~ /PASSWORD|PASSPHRASE|SECRET|TOKEN|KEY|CRED/) print }'
-fi
+echo Show Environment
+env | awk -F= '{ if (toupper($1) !~ /PASSWORD|PASSPHRASE|SECRET|TOKEN|KEY|CRED/) print }'
 
 cd ${ZLUX_APP_SERVER_DIR}/lib
 echo Starting node
